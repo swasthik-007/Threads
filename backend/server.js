f033513e3941bc43bfb8b4831c017725853705e8
swasthik-007 <mohanty.swastik4@gmail.com>
2024-06-18 22:51:33 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
+
+connectDB();
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
