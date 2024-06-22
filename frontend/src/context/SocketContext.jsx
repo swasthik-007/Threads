7ef6e6e1f6b468ed03659805503b56dcfab0df99
swasthik-007 <mohanty.swastik4@gmail.com>
2024-06-23 02:55:07 +0530

    cron.js added

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
     const user = useRecoilValue(userAtom);
 
     useEffect(() => {
-        const socket = io("https://threads1.onrender.com/", {
+        const socket = io("https://threads1.onrender.com", {
             query: {
                 userId: user?._id,
             },
